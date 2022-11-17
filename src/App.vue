76279e911c85f0d88a10ef96e51eb4b115df69a8
manuelmenama <manuel.mena090589@gmail.com>
2022-11-17 15:51:06 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppSearch from './components/AppSearch.vue';
+import AppCardWrapper from './components/AppCardWrapper.vue';
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppSearch,
+    AppCardWrapper
+  }
+}
+</script>
+
+<template>
+
+  <main>
+
+
+
+  </main>
+  
+</template>
+
+<style lang="scss">
+
+@use "./styles/general.scss";
+
+</style>
---
+++ src/components/AppCardWrapper.vue
@@ -0,0 +1,20 @@
+<script>
+
+import AppCard from './AppCard.vue';
+
+export default {
+  name: "AppCardWrapper",
+  components:{
+    AppCard
+  }
+}
+</script>
+
+<template>
+  
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
